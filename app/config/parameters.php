499aef23a56bc4c887f600add478492064798cfd
Unknown <taorai.asia@gmail.com>
2017-04-04 00:25:38 +0700

    init project

---
+++ app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'db_taorai_dev',
+    'database_user' => 'taorai_admin',
+    'database_password' => 'Szc2W2aVFDuHUCsX',
+    'database_prefix' => 'taorai_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'Wa9NTVneWFPgkX8H9QdjBnaEIn8KwAGFEGYBU2FZRbaNQsEPhu9gvzVJ',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2017-04-03',
+    'locale' => 'en-US',
+    'cookie_key' => 'yn828ag9G6KLDThr8t9Gh2wt0US5tYgzfiFrNFwx6VmiuVo8n9C83aEf',
+    'cookie_iv' => 'ABKL0v4O',
+    'new_cookie_key' => 'def00000d95bf632c2e20aca225a226ca80eb7edd304a79f00f40ea08cae78c7fc6eaacd6b7110c45ca7acf320b982c6e522450519dec9f60bb1b1e7598be81714cac2f5',
+  ),
+);
